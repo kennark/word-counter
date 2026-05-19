28767b5eaa423d082385de42e7f5ec2c15893d51
Kennar Kahju <68914924+kennark@users.noreply.github.com>
2026-05-19 15:47:59 +0300

    Removed unused methods

--- common/src/main/java/org/example/demo/common/repository/WordCountRepository.java
+++ common/src/main/java/org/example/demo/common/repository/WordCountRepository.java
@@ -3,7 +3,6 @@ package org.example.demo.common.repository;
 import org.example.demo.common.entity.WordCount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,9 +10,6 @@ import java.util.UUID;
 @Repository
 public interface WordCountRepository extends JpaRepository<WordCount, Long> {
     List<WordCount> findAllByTextIdEquals(UUID textId);
-
-    @Transactional
-    void deleteByTextIdEquals(UUID textId);
 
     List<WordCount> findAllByTextIdEqualsAndCountGreaterThan(UUID id, int i);
 }
--- worker/src/main/java/org/example/demo/worker/service/MetadataUpdater.java
+++ worker/src/main/java/org/example/demo/worker/service/MetadataUpdater.java
@@ -3,14 +3,10 @@ package org.example.demo.worker.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.demo.common.entity.TextUploadMetadata;
-import org.example.demo.common.entity.WordCount;
 import org.example.demo.common.repository.MetadataRepository;
 import org.example.demo.common.repository.WordCountRepository;
 import org.springframework.stereotype.Service;
-import tools.jackson.databind.ObjectMapper;
 
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 @Slf4j
@@ -28,18 +24,5 @@ public class MetadataUpdater {
         metadata.setProcessedChunks(metadata.getProcessedChunks() + 1);
 
         metadataRepository.save(metadata);
-    }
-
-    private String getJsonStringFromWordCountList(List<WordCount> wordCounts) {
-
-        List<Map<String, Object>> extract = wordCounts.stream()
-                .map(c ->
-                        Map.<String, Object>of(
-                                "word", c.getWord(), "count", c.getCount()
-                        ))
-                .toList();
-
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.writeValueAsString(extract);
     }
 }
